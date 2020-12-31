83265b4059f4bd482062a53cd73d92fe7ee235f9
Veronica Harris <s2vlharr@hotmail.com>
2020-12-31 10:12:20 -0500

    completed project

--- src/main/java/com/tts/yourdreamhouse/controller/YourDreamHouseController.java
+++ src/main/java/com/tts/yourdreamhouse/controller/YourDreamHouseController.java
@@ -2,11 +2,15 @@ package com.tts.yourdreamhouse.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
+import com.tts.yourdreamhouse.model.User;
 import com.tts.yourdreamhouse.model.YourDreamHouse;
 
 @Controller
@@ -46,10 +50,28 @@ public class YourDreamHouseController {
 	    public String victorian() {
 	        return "victorian";
 	    }
+		
+		
 					
 		@PostMapping("/questions")
 		public String questionsSubmit(@ModelAttribute YourDreamHouse yourDreamHouse, Model model) {
 			model.addAttribute("YourDreamHouse", yourDreamHouse);
 			return "questions";	
-		}			
+		}
+		
+		@RequestMapping(value="/registrationform", method = RequestMethod.GET)
+		public String answer(Model model) {
+			User user = new User();
+			YourDreamHouse dream = new YourDreamHouse();
+			model.addAttribute("user", user);
+			model.addAttribute("dream", dream);
+			return "registrationform";
+		}
+		
+		@PostMapping("/registrationform")
+		public String answerSubmit(@ModelAttribute("user") User user) {
+		    System.out.println("user: " + user);
+		    return "registrationform";
+		  }
+	
 }	
--- src/main/java/com/tts/yourdreamhouse/model/YourDreamHouse.java
+++ src/main/java/com/tts/yourdreamhouse/model/YourDreamHouse.java
@@ -1,81 +1,16 @@
 package com.tts.yourdreamhouse.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-@Entity
 public class YourDreamHouse {
 		
-		@Id
-		@GeneratedValue(strategy=GenerationType.AUTO)
-		private String answer1;
-		private String answer2;
-		private String answer3;
-		private String answer4;
-		private String answer5;
-		private String answer6;
-		private String answer7;
-		private String answer8;
-		
-		
-		public String getAnswer1() {
-			return answer1;
+		private String answer;
+
+		public String getAnswer() {
+			return answer;
 		}
-		public void setAnswer1(String answer1) {
-			this.answer1 = answer1;
+
+		public void setAnswer(String answer) {
+			this.answer = answer;
 		}
-		public String getAnswer2() {
-			return answer2;
-		}
-		public void setAnswer2(String answer2) {
-			this.answer2 = answer2;
-		}
-		public String getAnswer3() {
-			return answer3;
-		}
-		public void setAnswer3(String answer3) {
-			this.answer3 = answer3;
-		}
-		public String getAnswer4() {
-			return answer4;
-		}
-		public void setAnswer4(String answer4) {
-			this.answer4 = answer4;
-		}
-		public String getAnswer5() {
-			return answer5;
-		}
-		public void setAnswer5(String answer5) {
-			this.answer5 = answer5;
-		}
-		public String getAnswer6() {
-			return answer6;
-		}
-		public void setAnswer6(String answer6) {
-			this.answer6 = answer6;
-		}
-		public String getAnswer7() {
-			return answer7;
-		}
-		public void setAnswer7(String answer7) {
-			this.answer7 = answer7;
-		}
-		public String getAnswer8() {
-			return answer8;
-		}
-		public void setAnswer8(String answer8) {
-			this.answer8 = answer8;
-		}
-		@Override
-		public String toString() {
-			return "YourDreamHouse [answer1=" + answer1 + ", answer2=" + answer2 + ", answer3=" + answer3 + ", answer4="
-					+ answer4 + ", answer5=" + answer5 + ", answer6=" + answer6 + ", answer7=" + answer7 + ", answer8="
-					+ answer8 + "]";
-		}
-		
-		
-		
 		
 }
